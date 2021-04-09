29ee4ce08d0e6232b54cb19c86860d0cd3a95d6c
Sally Cao <scao14@jhu.edu>
2021-04-09 09:36:52 -0400

    added checkbox for consent

--- react-frontend/src/pages/StarterPage/StarterPage.js
+++ react-frontend/src/pages/StarterPage/StarterPage.js
@@ -1,26 +1,51 @@
+import React, { useState } from 'react';
+
 import { startReq } from '../../utils/request.js';
-import { Button } from "antd";
+import { Button, Checkbox, message} from "antd";
+
+import "./css/starterPage.css";
 
 const StarterPage = () => {
 
-  const start = () => {
-    console.log("start study")
-    startReq({ userApproval: "True"}).then(window.location.assign("/#/mainpage"))
-  };
+    const [consent, setConsent] = useState(false);
 
-  return (
-    <div className="page-container">
-      <div className="title">Counting Penguins with AI assistance</div>
-      <div>
-        <Button
-            type="primary"
-            onClick={() => start()}
-        >
-          Start Study
-        </Button>
-      </div>
-    </div>
-  );
+    const start = () => {
+        if(consent === true){
+            console.log("start study")
+            startReq({ userApproval: "True"}).then(window.location.assign("/#/mainpage"))
+        }
+        else{
+            message.warning('Need to give consent to participate in the study.');
+        }
+    };
+
+    function onChange(e) {
+        console.log(`consent = ${e.target.checked}`);
+        setConsent(e.target.checked);
+    }
+
+    return (
+        <div className="page-container">
+          <div className="title">Counting Penguins with AI assistance</div>
+
+            <div className='checkboxMessage'>
+                <Checkbox onChange={onChange}>
+                    I give my consent for my data to be used for the study
+                </Checkbox>
+            </div>
+
+            <div>
+                <Button
+                    type="primary"
+                    size="medium"
+                    style={{ margin: '0 10px' }}
+                    onClick={() => start()}
+                >
+                    Start Study
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default StarterPage;
