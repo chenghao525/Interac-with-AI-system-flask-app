4f5d31d297e5dd9319f73f62db24e727ecf0b6bc
Sally Cao <scao14@jhu.edu>
2021-04-12 11:52:18 -0400

    notes on data tables

--- api/buildDB.py
+++ api/buildDB.py
@@ -5,7 +5,25 @@ conn = sql.connect('./database.db')
 print("Opened database successfully")
 cur = conn.cursor()
 
+# User Table:
+# 1. User ID == Row Number
+# 2. Consent: Integer
+# 3. Condition: String (EF or HF)
+# 4. Time Constraint: String (S or L)
 conn.execute('CREATE TABLE Consent (consent INTEGER NOT NULL)')
+
+# User Initial Guess Table
+# 1. User ID: Integer
+# 2. Question ID: String (E1 or H3)
+# 3. Initial Guess: Integer
+# 4. Final Guess: Integer
+# 5. Response Time: Integer (in seconds)
+
+# Image Table
+# 1. Question ID: String
+# 2. Image Path: String
+# 3. Ground Truth: Integer
+# 4. AI Guess: Integer
 
 # Test Add Value
 consent = -1
