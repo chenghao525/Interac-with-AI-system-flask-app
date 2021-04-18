fcee4b63723361023cdd448b8130081095ee3c6d
Sally Cao <scao14@jhu.edu>
2021-04-18 11:48:48 -0400

    added consent statement

--- react-frontend/src/pages/StarterPage/StarterPage.js
+++ react-frontend/src/pages/StarterPage/StarterPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 
 import { startReq } from '../../utils/request.js';
-import { Button, Checkbox, message} from "antd";
+import { Button, Checkbox, message, Typography} from "antd";
 
 import "./css/starterPage.css";
+
+const { Title,  Text } = Typography;
 
 const StarterPage = () => {
 
@@ -26,8 +28,9 @@ const StarterPage = () => {
 
     return (
         <div className="page-container">
-          <div className="title">Counting Penguins with AI assistance</div>
-
+          <Title level={3}>Counting Penguins with AI assistance</Title>
+            <Text level={5}>You are being asked to join a study for a course project at Johns Hopkins University. Participation in this study is voluntary and you will not receive any compensation. Even if you decide to join now, you can change your mind later. If you want to stop your participation in the study, you can simply close the browser. The  study tasks can be completed within 8-10 minutes. The records and data from the study will only be reviewed by team members involved in the project. We will not ask for information that identifies you, thus, your responses cannot be associated with your identity. By clicking next you accept these conditions, and you agree to  join the study.
+            </Text>
             <div className='checkboxMessage'>
                 <Checkbox onChange={onChange}>
                     I give my consent for my data to be used for the study
