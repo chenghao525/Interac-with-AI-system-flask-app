e060a9e80889b935a2ed5e6c35f22b486d36ff71
Sally Cao <scao14@jhu.edu>
2021-04-19 10:23:33 -0400

    try adding app.py for deployment

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.main import main
+from api.main import start, getUserData, inputAnswer, getImageInfo
 
-app = Flask(__name__, static_url_path='', static_folder='react-frontend/build')
+app = Flask(__name__, static_folder='react-frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
-api.add_resource(main, '/flask/hello')
+api.add_resource(start, '/start')
+api.add_resource(getUserData, '/userInfo')
+api.add_resource(inputAnswer, '/answer')
+api.add_resource(getImageInfo, '/imageInfo')
