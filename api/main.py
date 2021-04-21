f4162678c791972eda898f580cb0a62cf84561c1
Sally Cao <scao14@jhu.edu>
2021-04-21 10:17:51 -0400

    changed port

--- api/main.py
+++ api/main.py
@@ -154,6 +154,6 @@ def getImageInfo():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=False, port=port)
+    app.run(host='127.0.0.1', debug=False, port=port)
 
 
