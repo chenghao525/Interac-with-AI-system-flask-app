9b6c7f40bf9aafdd5376288eed15b4f63665e263
Sally Cao <scao14@jhu.edu>
2021-04-24 21:09:32 -0400

    temp database-url

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 #cors = CORS(app)
 
 # Get Database_url from Heroku config and add sslmode to query
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = 'postgres://ststorukymhbaj:643f2dd332c333cf2d659de25a99ee0a697a2ac9d14933a362ceb061ad9ce9ad@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d9pmgqc5g5r6go'
 params = {'sslmode': 'require'}
 url_parts = list(urlparse.urlparse(DATABASE_URL))
 query = dict(urlparse.parse_qsl(url_parts[4]))
